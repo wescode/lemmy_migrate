d769ac0bf76c2ab234e1f3932627384dc0dead99
me <whickle@gmail.com>
2023-06-16 11:07:42 -0400

    add arguments

--- lemmy.py
+++ lemmy.py
@@ -1,15 +1,18 @@
+import sys
 import requests
 from collections import defaultdict
+from time import sleep
 
 class Lemmy:
-    _site_url = None
-    _auth_token = None
+    
     _api_version = "v3"
     _api_base_url = f"api/{_api_version}"
-    _user_communities = defaultdict(dict)
-
+    
     def __init__(self, url) -> None:
         self._site_url = url
+        self._auth_token = None
+        self._user_communities = defaultdict(dict)
+        self._requests_total = 0
     
     def login(self, user, password):
         '''authenticate to instance'''
@@ -21,9 +24,13 @@ class Lemmy:
         try:
             resp = requests.post(f"{self._site_url}/{self._api_base_url}/user/login",
                              json=payload)
+            resp.raise_for_status()
             self._auth_token = resp.json()['jwt']
-        except Exception as err:
-            print(f"{err}")
+        except requests.exceptions.HTTPError as ex:
+            print(f"   HTTP error: {ex.response}, while authenticating")
+            raise Exception(f"HTTP error: {ex.response} while authenticating")
+        except requests.exceptions.RequestException as ex:
+            raise Exception(f"Error: {ex} while authenticating")
 
     def get_communities(self):
         '''Get list of currently subscribed communites'''
@@ -38,6 +45,8 @@ class Lemmy:
             # iterate over each page if needed
             fetched = 50 #max limit
             while fetched == 50:
+                # rate limit: 60 messages in 6s
+                self._requests_total += 1
                 resp = requests.get(f"{self._site_url}/{self._api_base_url}/community/list",
                                     params=payload)
                 fetched = len(resp.json()['communities'])
@@ -75,6 +84,10 @@ class Lemmy:
                 if comm_id:
                     payload['community_id'] = comm_id
                     print(f"   Subscribing to {url['name']} ({comm_id})")
+                    self._requests_total += 1
+                    print(f"   Total request: {self._requests_total}")
+                    # rate limit: 60 messages in 6s
+                    self.rate_limit()
                     resp = requests.post(f"{self._site_url}/{self._api_base_url}/community/follow",
                                         json=payload)
                     resp.raise_for_status()
@@ -92,8 +105,11 @@ class Lemmy:
 
         community_id = None
         try:
+            # rate limit: 60 messages in 6s
+            self._requests_total += 1
             resp = requests.get(f"{self._site_url}/{self._api_base_url}/resolve_object",
                                 params=payload)
+            resp.raise_for_status()
             community_id = resp.json()['community']['community']['id']
         except requests.exceptions.HTTPError as ex:
             print(f"   HTTP error: {ex.response}, while fetching {community}")
@@ -101,3 +117,12 @@ class Lemmy:
             print(f"   Error: {ex}")
 
         return community_id
+    
+    def rate_limit(self):
+        return
+        if self._requests_total >= 20:
+            print("   Rate limited, sleep for 6s...")
+            sleep(6)
+            self._requests_total = 1
+        
+        return
