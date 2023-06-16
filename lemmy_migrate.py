31902d9a1ed5f181c0a697fa4b00ffcad6086ab2
me <whickle@gmail.com>
2023-06-16 11:08:07 -0400

    f

--- lemmy_migrate.py
+++ lemmy_migrate.py
@@ -1,9 +1,24 @@
+import argparse
 from lemmy import Lemmy
 
-lemming = Lemmy('https://sh.itjust.works')
-lemming.login()
+parser = argparse.ArgumentParser(
+                    prog='lemmy_migrate',
+                    description='Migrate subscribed communities from one account to another')
+parser.add_argument('-s', required=True, help='Source site URL', metavar='source_site')
+parser.add_argument('-a', required=True, help='Source account', metavar='source_account')
+parser.add_argument('-p', required=True, help='Source password', metavar='source_password')
+parser.add_argument('-d', help='Destination site URL', required=True, metavar='destination_site')
+parser.add_argument('-l', help='Destination account', required=True, metavar='destination_account')
+parser.add_argument('-c', help='Destination password', required=True, metavar='destination_password')
+args = parser.parse_args()
+
+# source site
+lemming = Lemmy(args.s)
+lemming.login(args.a, args.p)
 communties = lemming.get_communities()
 
-new_lemming = Lemmy('https://infosec.pub')
-new_lemming.login()
+# destination site
+new_lemming = Lemmy(args.d)
+new_lemming.login(args.l, args.c)
+new_lemming.get_communities()
 new_lemming.subscribe(communties)
