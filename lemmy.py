e8840c9befb227f5c8549a431c33d0b0bb81e202
me <whickle@gmail.com>
2023-06-21 15:49:44 -0400

    use config file now

--- lemmy.py
+++ lemmy.py
@@ -12,7 +12,6 @@ class Lemmy:
         self._site_url = url
         self._auth_token = None
         self._user_communities = defaultdict(dict)
-        self._requests_total = 0
     
     def login(self, user: str, password: str) -> None:
         """authenticate to instance"""
@@ -27,12 +26,15 @@ class Lemmy:
                 method='POST', json=payload)
             self._auth_token = resp.json()['jwt']
         except Exception as e:
-            raise Exception(f"Failed to authenticate: {e}")
+          #raise Exception(f"Failed to authenticate: {e}")
+          self._println(1, f"[ERROR]: login() failed for {user} on {self._site_url}")
+          self._println(2, f"-Details: {e}")
+          sys.exit(1)
             
-    def get_communities(self) -> dict:
+    def get_communities(self, type: str = "Subscribed") -> dict:
         """Get list of currently subscribed communites"""
         payload = { 
-            'type_': 'Subscribed',
+            'type_': type,
             'auth': self._auth_token,
             'limit': 50,
             'page': 1
@@ -41,7 +43,6 @@ class Lemmy:
         # iterate over each page if needed
         fetched = 50 #max limit
         while fetched == 50:
-            self._requests_total += 1
             try:
                 resp = self._request_it(
                     f"{self._site_url}/{self._api_base_url}/community/list",
@@ -52,14 +53,14 @@ class Lemmy:
                 for comm in resp.json()['communities']:
                     id = comm['community']['id']
                     url = comm['community']['actor_id']
-                    self._user_communities[id]['name'] = url
+                    self._user_communities[url]['id'] = id
             except Exception as err:
                 print(f"error: {err}")
         
         return self._user_communities
 
     def subscribe(self, communities: dict = None) -> None:
-        """Subscribe to a community. It will first attempt to fetch community."""
+        """Subscribe to a community. It will first attempt to resolve community."""
         if communities:
             self._user_communities = communities
         else:
@@ -71,63 +72,58 @@ class Lemmy:
             'auth': self._auth_token
         }
 
-        for cid,url in self._user_communities.items():
+        for url,cid in self._user_communities.items():
             try: 
                 # resolve community first
-                print(f"Fetching {url['name']}")
-                comm_id = self.fetch_community(url['name'])
+                comm_id = self.resolve_community(url)
                 
                 if comm_id:
                     payload['community_id'] = comm_id
-                    print(f"   Subscribing to {url['name']} ({comm_id})")
-                    self._requests_total += 1
-                    #print(f"   Total request: {self._requests_total}")
-                    self._rate_limit()
+                    self._println(2, f"> Subscribing to {url} ({comm_id})")
                     resp = self._request_it(
                         f"{self._site_url}/{self._api_base_url}/community/follow",
                         json=payload, method='POST')
                     
                     if resp.status_code == 200:
-                        print(f"   Succesfully subscribed to {url['name']} ({comm_id})")
+                        self._println(3, f"> Succesfully subscribed to {url} ({comm_id})")
             except Exception as e:
                 print(f"   API error: {e}")
 
-    def fetch_community(self, community: str) -> int | None:
-        """Fetch a community"""
+    def resolve_community(self, community: str) -> int | None:
+        """resolve a community"""
         payload = {
             'q': community,
             'auth':self._auth_token
         }
 
         community_id = None
+        self._println(1, f"> Resolving {community}")
         try:
-            self._requests_total += 1
-            self._rate_limit()
             resp = self._request_it(
                 f"{self._site_url}/{self._api_base_url}/resolve_object",
                 params=payload)
             community_id = resp.json()['community']['community']['id']
         except Exception as e:
-            print(f"   Failed to fetch community {e}")
+            self._println(2, f"> Failed to resolve community {e}")
 
         return community_id
     
     def _rate_limit(self):
-        #if self._requests_total >= 15:
-        #    print("   Rate limited, sleep for 10s...")
         sleep(1)
-        #    self._requests_total = 1
     
     def _request_it(
             self, endpoint: str, method: str = 'GET', params: str = None,
             json: dict = None) -> requests.Response:
+            
+        self._rate_limit()
         try:
             r = requests.request(method, url=endpoint, params=params, json=json)
             r.raise_for_status()
             return r
         except requests.exceptions.HTTPError as e:
-            #raise Exception(f"Error at endpoint {endpoint}: {ex}")
-            raise Exception("Error") from e
+            raise
         except requests.exceptions.RequestException as e:
-            raise Exception(e)
-            #raise Exception(f"Error at endpoint {endpoint}: {ex}")
+            raise
+
+    def _println(self, indent, line):
+        print(f"{' ' * indent}{line}")
--- lemmy_migrate.py
+++ lemmy_migrate.py
@@ -1,24 +1,52 @@
+import sys
 import argparse
+import configparser
 from lemmy import Lemmy
 
-parser = argparse.ArgumentParser(
-                    prog='lemmy_migrate',
-                    description='Migrate subscribed communities from one account to another')
-parser.add_argument('-s', required=True, help='Source site URL', metavar='source_site')
-parser.add_argument('-a', required=True, help='Source account', metavar='source_account')
-parser.add_argument('-p', required=True, help='Source password', metavar='source_password')
-parser.add_argument('-d', help='Destination site URL', required=True, metavar='destination_site')
-parser.add_argument('-l', help='Destination account', required=True, metavar='destination_account')
-parser.add_argument('-c', help='Destination password', required=True, metavar='destination_password')
-args = parser.parse_args()
+def get_config(cfile):
+    config = configparser.ConfigParser()
+    read = config.read(cfile)
+    if not read:
+        print(f"Could not read config {cfile}!")
+        sys.exit(1)
 
-# source site
-lemming = Lemmy(args.s)
-lemming.login(args.a, args.p)
-communties = lemming.get_communities()
+    accounts = {i: dict(config.items(i)) for i in config.sections()}
+    return accounts
 
-# destination site
-new_lemming = Lemmy(args.d)
-new_lemming.login(args.l, args.c)
-new_lemming.get_communities()
-new_lemming.subscribe(communties)
+def get_args():
+    parser = argparse.ArgumentParser(
+        prog='lemmy_migrate',
+        description='Migrate subscribed communities from one account to another')
+
+    parser.add_argument('-c', required=True, help="Path to config file",
+                        metavar="config_file") 
+    args = parser.parse_args()
+    return args.c
+
+def main():
+    cfg = get_args()
+    accounts = get_config(cfg)
+
+    # source site
+    print(f"\n[ Getting Main Account info - {accounts['Main Account']['site']} ]")
+    lemming = Lemmy(accounts['Main Account']['site'])
+    lemming.login(accounts['Main Account']['user'], 
+                accounts['Main Account']['password'])
+    communties = lemming.get_communities()
+    print(f" {len(communties)} subscribed communities found")
+    accounts.pop('Main Account', None)
+
+    # sync main account communities to each account
+    for acc in accounts:
+        print(f"\n[ Syncing {acc} - {accounts[acc]['site']} ]")
+        new_lemming = Lemmy(accounts[acc]['site'])
+        new_lemming.login(accounts[acc]['user'], accounts[acc]['password'])
+        comms = new_lemming.get_communities()
+        print(f" {len(comms)} subscribed communities found")
+        new_communities = {c: communties[c] for c in communties if c not in comms}
+        
+        print(f" Subscribing to {len(new_communities)} new communities")
+        new_lemming.subscribe(new_communities)
+
+if __name__ == "__main__":
+    main()
